834180106242d0ab0a169ce1cedcc7785d7cdfae
medc1610 <medc1610@gmail.com>
2023-08-30 21:03:46 -0400

    CSS to SCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { filter, takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'Angular 11 - MSAL v2 Quickstart Sample';
